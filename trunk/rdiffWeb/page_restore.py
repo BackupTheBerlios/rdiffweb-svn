917200bf2a1de336d39fc1b0fd9f3d53575552b3
joshn <joshn@d705d8ab-5c0b-0410-ae88-ad3e7cc979f9>
2006-02-09 03:33:01 +0000

    Disabling restores while a backup is in progress

--- trunk/rdiffWeb/page_restore.py
+++ trunk/rdiffWeb/page_restore.py
@@ -26,6 +26,9 @@ class rdiffRestorePage(page_main.rdiffPage):
       if not repo in self.userDB.getUserRepoPaths(self.getUsername()):
          return self.writeErrorPage("Access is denied.")
 
+      if librdiff.backupIsInProgress(rdw_helpers.joinPaths(self.userDB.getUserRoot(self.getUsername()), repo)):
+         return self.writeErrorPage("A backup is currently in progress to this location.  Restores are disabled until this backup is complete.")
+
       try:
          restoreTime = rdw_helpers.rdwTime()
          restoreTime.initFromString(date)
