8b60b688af1c8b85f4ea2539f843dc3275f5e104
joshn <joshn@d705d8ab-5c0b-0410-ae88-ad3e7cc979f9>
2006-02-10 00:25:08 +0000

    Incrementing version and correcting contact information

--- trunk/setup.py
+++ trunk/setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='rdiffWeb',
-      version='0.1',
+      version='0.2',
       description='A web interface to rdiff-backup repositories',
       author='Josh Nisly',
-      author_email='rdiffweb@joshnisly.com',
-      url='http://www.joshnisly.com/rdiffweb',
+      author_email='rdiffweb@rdiffweb.org',
+      url='http://www.rdiffweb.org',
       packages=['rdiffWeb'],
       package_data={'rdiffWeb': ['templates/*.html', 'static/*']},
       data_files=[('/etc/rdiffweb', ['rdw.conf.sample']),
