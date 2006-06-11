d721f31929ae7c8878fe5b2a1cc128eaac7e27e0
joshn <joshn@d705d8ab-5c0b-0410-ae88-ad3e7cc979f9>
2006-06-11 17:48:43 +0000

    Version bump

--- trunk/setup.py
+++ trunk/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='rdiffWeb',
-      version='0.3.1',
+      version='0.3.2',
       description='A web interface to rdiff-backup repositories',
       author='Josh Nisly',
       author_email='rdiffweb@rdiffweb.org',
