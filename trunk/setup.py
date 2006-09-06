392ad3df4ec9398a8903a84a39486cc9f8420ec3
joshn <joshn@d705d8ab-5c0b-0410-ae88-ad3e7cc979f9>
2006-09-06 17:46:57 +0000

    Fix installion on python verions <= 2.3

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from distutils.core import setup, Command
+import glob, sys
 
-setup(name='rdiffWeb',
+# < Python 2.4 does not have the package_data setup keyword, making installation a pain.
+# If we can, use package_data, otherwise, hack in the files
+pythonVersion = sys.version_info[0]+sys.version_info[1]/10.0
+if pythonVersion > 2.3:
+   setup(name='rdiffWeb',
       version='0.3.2',
       description='A web interface to rdiff-backup repositories',
       author='Josh Nisly',
@@ -12,6 +17,30 @@ setup(name='rdiffWeb',
       package_data={'rdiffWeb': ['templates/*.html', 'templates/*.xml', 'static/*']},
       data_files=[('/etc/rdiffweb', ['rdw.conf.sample']),
                   ('/etc/init.d', ['init/rdiff-web'])
+                  ],
+      scripts=['rdiff-web', 'rdiff-web-config']
+     )
+else:
+   from distutils.dist import Distribution
+   import distutils.command.install
+
+   installer = distutils.command.install.install(Distribution())
+   installer.initialize_options()
+   installer.finalize_options()
+   packageDataDir = installer.config_vars['prefix']+"/lib/python"+installer.config_vars['py_version_short']+"/site-packages/rdiffWeb"
+
+   setup(name='rdiffWeb',
+      version='0.3.2',
+      description='A web interface to rdiff-backup repositories',
+      author='Josh Nisly',
+      author_email='rdiffweb@rdiffweb.org',
+      url='http://www.rdiffweb.org',
+      packages=['rdiffWeb'],
+      data_files=[('/etc/rdiffweb', ['rdw.conf.sample']),
+                  ('/etc/init.d', ['init/rdiff-web']),
+                  (packageDataDir+'/templates', glob.glob('rdiffWeb/templates/*.html')),
+                  (packageDataDir+'/templates', glob.glob('rdiffWeb/templates/*.xml')),
+                  (packageDataDir+'/static', glob.glob('rdiffWeb/static/*')),
                   ],
       scripts=['rdiff-web', 'rdiff-web-config']
      )
