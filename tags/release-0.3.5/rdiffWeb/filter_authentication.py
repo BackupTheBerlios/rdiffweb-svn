d652facd36c83a714bb67a3d3de4838836f3dd10
joshn <joshn@d705d8ab-5c0b-0410-ae88-ad3e7cc979f9>
2006-10-30 19:46:43 +0000

    Tagging version 0.3.5

---
+++ tags/release-0.3.5/rdiffWeb/filter_authentication.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+
+import cherrypy
+try:
+   from cherrypy.filters.basefilter import BaseFilter
+except:
+   from cherrypy.lib.filter.basefilter import BaseFilter
+import page_main
+import rdw_helpers
+import base64
+
+class rdwAuthenticationFilter(BaseFilter):
+   loginUrl = "/doLogin"
+   logoutUrl = "/doLogout"
+   sessionUserNameKey = "username"
+
+   def beforeMain(self):
+      if not cherrypy.config.get("rdwAuthenticateFilter.on", False):
+         return
+      checkLoginAndPassword = cherrypy.config.get("rdwAuthenticateFilter.checkLoginAndPassword", lambda username, password: u"Invalid username or password")
+
+      if cherrypy.request.path == self.logoutUrl:
+         cherrypy.session[self.sessionUserNameKey] = None
+         cherrypy.request.user = None
+         raise cherrypy.HTTPRedirect("/")
+
+      elif cherrypy.session.get(self.sessionUserNameKey):
+         # page passes credentials; allow to be processed
+         if cherrypy.request.path == self.loginUrl:
+            raise cherrypy.HTTPRedirect("/")
+         return
+
+      if cherrypy.config.get("rdwAuthenticateFilter.method", "") == "HTTP Header":
+         # if not already authenticated, authenticate via the Authorization header
+         httpAuth = self._getHTTPAuthorizationCredentials(cherrypy.request.headerMap.get("Authorization", ""))
+         if httpAuth:
+            error = checkLoginAndPassword(httpAuth["login"], httpAuth["password"])
+            if not error:
+               return
+         else:
+            error = ""
+
+         cherrypy.response.status = "401 Unauthorized"
+         cherrypy.response.body = "Not Authorized\n" + error
+         cherrypy.response.headerMap["WWW-Authenticate"] = 'Basic realm="cherrypy"'
+         return
+
+      loginKey = "login"
+      passwordKey = "password"
+      redirectKey = "redirect"
+
+      loginParms = {"message": "", "action": self.loginUrl,
+         "loginKey": loginKey, "passwordKey": passwordKey, "redirectKey": redirectKey,
+         "loginValue": "", "redirectValue": cherrypy.request.path + "?" + cherrypy.request.queryString }
+
+      if cherrypy.request.path == self.loginUrl and cherrypy.request.method == "POST":
+         # check for login credentials
+         loginValue = cherrypy.request.paramMap[loginKey]
+         passwordValue = cherrypy.request.paramMap[passwordKey]
+         redirectValue = cherrypy.request.paramMap[redirectKey]
+         errorMsg = checkLoginAndPassword(loginValue, passwordValue)
+         if not errorMsg:
+            cherrypy.session[self.sessionUserNameKey] = loginValue
+            if not redirectValue:
+               redirectValue = "/"
+            raise cherrypy.HTTPRedirect(redirectValue)
+
+         # update form values
+         loginParms["message"] = errorMsg
+         loginParms["loginValue"] = loginValue
+         loginParms["redirectValue"] = redirectValue
+
+      # write login page
+      loginPage = page_main.rdiffPage()
+      cherrypy.response.body = loginPage.compileTemplate("login.html", **loginParms)
+      cherrypy.request.execute_main = False      
+
+   def _getHTTPAuthorizationCredentials(self, authHeader):
+      try:
+         (realm, authEnc) = authHeader.split()
+      except ValueError:
+         return None
+
+      if realm.lower() == "basic":
+         auth = base64.decodestring(authEnc)
+         colon = auth.find(":")
+         if colon != -1:
+            return { "login": auth[:colon], "password": auth[colon+1:] }
+         else:
+            return { "login": auth, "password": "" }
+
+      return None
+
+##################### Unit Tests #########################
+
+import unittest, os
+class rdwAuthenticationFilterTest(unittest.TestCase):
+   """Unit tests for the rdwAuthenticationFilter class"""
+
+   def testAuthorization(self):
+      filter = rdwAuthenticationFilter()
+      assert not filter._getHTTPAuthorizationCredentials("")
+      assert not filter._getHTTPAuthorizationCredentials("Basic Username Password")
+      assert not filter._getHTTPAuthorizationCredentials("Digest " + base64.encodestring("username"))
+      assert filter._getHTTPAuthorizationCredentials("Basic " + base64.encodestring("username")) == { "login": "username", "password": "" }
+      assert filter._getHTTPAuthorizationCredentials("Basic " + base64.encodestring("user:pass")) == { "login": "user", "password": "pass" }
+      assert filter._getHTTPAuthorizationCredentials("Basic " + base64.encodestring("user:pass:word")) == { "login": "user", "password": "pass:word" }
+
+if __name__ == "__main__":
+   print "Called as standalone program; running unit tests..."
+   fileUserDataTest = unittest.makeSuite(rdwAuthenticationFilterTest, 'test')
+   testRunner = unittest.TextTestRunner()
+   testRunner.run(fileUserDataTest)
---
+++ tags/release-0.3.5/rdiffWeb/page_status.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python
+
+import page_main
+import librdiff
+import rdw_helpers
+import cherrypy
+
+class rdiffStatusPage(page_main.rdiffPage):
+   def index(self):
+      userMessages = self._getRecentUserMessages()
+      page = self.startPage("Backup Status", rssUrl=self._buildStatusFeedUrl(), rssTitle = "Backup status for "+self.getUsername())
+      page = page + self.writeTopLinks()
+      page = page + self.compileTemplate("status.html", messages=userMessages, feedLink=self._buildStatusFeedUrl(), statusLink="", title="Backup Status")
+      page = page + self.endPage()
+      return page
+   index.exposed = True
+
+   def entry(self, date="", repo=""):
+      # Validate date
+      try:
+         entryTime = rdw_helpers.rdwTime()
+         entryTime.initFromString(date)
+      except ValueError:
+         return self.writeErrorPage("Invalid date parameter.")
+
+      if not repo:
+         userRepos = self.userDB.getUserRepoPaths(self.getUsername())
+
+         # Set the start and end time to be the start and end of the day, respectively, to get all entries for that day
+         startTime = rdw_helpers.rdwTime()
+         startTime.timeInSeconds = entryTime.timeInSeconds
+         startTime.tzOffset = entryTime.tzOffset
+         endTime = rdw_helpers.rdwTime()
+         endTime.timeInSeconds = entryTime.timeInSeconds
+         endTime.tzOffset = entryTime.tzOffset
+         startTime.setTime(0, 0, 0)
+         endTime.setTime(23, 59, 59)
+
+         userMessages = self._getUserMessages(userRepos, True, False, startTime, endTime)
+      else:
+         # Validate repo parameter
+         if not repo: return self.writeErrorPage("Backup location not specified.")
+         if not repo in self.userDB.getUserRepoPaths(self.getUsername()):
+            return self.writeErrorPage("Access is denied.")
+         try:
+            rdw_helpers.ensurePathValid(repo)
+         except rdw_helpers.accessDeniedError, error:
+            return self.writeErrorPage(str(error))
+
+         userMessages = self._getUserMessages([repo], False, True, entryTime, entryTime)
+
+      page = self.startPage("Backup Status Entry", rssUrl="", rssTitle="")
+      page = page + self.writeTopLinks()
+      page = page + self.compileTemplate("status.html", messages=userMessages, feedLink="", statusLink=self._buildAbsoluteStatusUrl(), title="Backup Status Entry")
+      page = page + self.endPage()
+      return page
+   entry.exposed = True
+
+   def feed(self):
+      cherrypy.response.headerMap["Content-Type"] = "text/xml"
+      userMessages = self._getRecentUserMessages()
+      statusUrl = self._buildAbsoluteStatusUrl()
+      return self.compileTemplate("status.xml", username=self.getUsername(), link=statusUrl, messages=userMessages)
+   feed.exposed = True
+
+   def _buildAbsoluteStatusUrl(self):
+      return cherrypy.request.base + "/status/"
+
+   def _buildStatusFeedUrl(self):
+      return "/status/feed"
+
+   def _buildStatusEntryUrl(self, repo, date):
+      return self._buildAbsoluteStatusUrl() + "entry?repo="+rdw_helpers.encodeUrl(repo)+"&date="+rdw_helpers.encodeUrl(date.getUrlString())
+
+   def _getRecentUserMessages(self):
+      userRepos = self.userDB.getUserRepoPaths(self.getUsername())
+      asOfDate = rdw_helpers.rdwTime()
+      asOfDate.initFromMidnightUTC(-5)
+
+      return self._getUserMessages(userRepos, True, True, asOfDate, None)
+
+   def _getUserMessages(self, repos, includeSuccess, includeFailure, earliestDate, latestDate):
+      userRoot = self.userDB.getUserRoot(self.getUsername())
+
+      repoErrors = []
+      allBackups = []
+      for repo in repos:
+         try:
+            backups = librdiff.getBackupHistoryForDateRange(rdw_helpers.joinPaths(userRoot, repo), earliestDate, latestDate);
+            allBackups += [{"repo": repo, "date": backup.date, "displayDate": backup.date.getDisplayString(),
+               "size": rdw_helpers.formatFileSizeStr(backup.size), "errors": backup.errors} for backup in backups]
+         except librdiff.FileError, error:
+            repoErrors.append({"repo": repo, "error": error.getErrorString()})
+
+      allBackups.sort(lambda x, y: cmp(y["date"], x["date"]))
+      failedBackups = filter(lambda x: x["errors"], allBackups)
+
+      # group successful backups by day
+      successfulBackups = filter(lambda x: not x["errors"], allBackups)
+      if successfulBackups:
+         lastSuccessDate = successfulBackups[0]["date"]
+      successfulBackups = rdw_helpers.groupby(successfulBackups, lambda x: x["date"].getLocalDaysSinceEpoch())
+
+      userMessages = []
+
+      # generate failure messages
+      if includeFailure:
+         for job in failedBackups:
+            date = job["date"]
+            title = "Backup Failed: " + job["repo"]
+            job.update({"isSuccess": False, "date": date, "pubDate": date.getRSSPubDateString(),
+               "link": self._buildStatusEntryUrl(job["repo"], date), "title": title, "repoErrors": [], "backups": []})
+            userMessages.append(job)
+
+      # generate success messages (publish date is most recent backup date)
+      if includeSuccess:
+         for day in successfulBackups.keys():
+            date = successfulBackups[day][0]["date"]
+            title = "Successful Backups for " + date.getDateDisplayString()
+
+            # include repository errors in most recent entry
+            if date == lastSuccessDate: repoErrorsForMsg = repoErrors
+            else: repoErrorsForMsg = []
+
+            userMessages.append({"isSuccess": 1, "date": date, "pubDate": date.getRSSPubDateString(),
+               "link": self._buildStatusEntryUrl("", date), "title": title, "repoErrors": repoErrorsForMsg, "backups":successfulBackups[day]})
+
+      # sort messages by date
+      userMessages.sort(lambda x, y: cmp(y["date"], x["date"]))
+      return userMessages
