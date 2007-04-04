7e84c5dd728cb5c6184e73620e7e900aad2bb4d5
joshn <joshn@d705d8ab-5c0b-0410-ae88-ad3e7cc979f9>
2007-04-04 21:20:52 +0000

    Show invalid repositories at the bottom of the repo listing page

--- trunk/rdiffWeb/page_locations.py
+++ trunk/rdiffWeb/page_locations.py
@@ -4,25 +4,33 @@ import rdw_helpers, page_main, librdiff
 
 
 class rdiffLocationsPage(page_main.rdiffPage):
+   ''' Shows the locations page. Will show all available destination
+   backup directories. This is the root (/) page '''
    def index(self):
       page = self.startPage("Backup Locations")
 
       repoList = []
+      repoErrors = []
       for userRepo in self.userDB.getUserRepoPaths(self.getUsername()):
          try:
             repoHistory = librdiff.getLastBackupHistoryEntry(rdw_helpers.joinPaths(self.userDB.getUserRoot(self.getUsername()), userRepo))
          except librdiff.FileError:
             repoSize = "0"
             repoDate = "Error"
-         else:
-            repoSize = rdw_helpers.formatFileSizeStr(repoHistory.size)
-            repoDate = repoHistory.date.getDisplayString()
-         repoList.append({ "repoName" : userRepo,
+            repoErrors.append({ "repoName" : userRepo,
                            "repoSize" : repoSize,
                            "repoDate" : repoDate,
                            "repoBrowseUrl" : self.buildBrowseUrl(userRepo, "/", False),
                            "repoHistoryUrl" : self.buildHistoryUrl(userRepo) })
-      page = page + self.compileTemplate("repo_listing.html", title="browse", repos=repoList)
+         else:
+            repoSize = rdw_helpers.formatFileSizeStr(repoHistory.size)
+            repoDate = repoHistory.date.getDisplayString()
+            repoList.append({ "repoName" : userRepo,
+                              "repoSize" : repoSize,
+                              "repoDate" : repoDate,
+                              "repoBrowseUrl" : self.buildBrowseUrl(userRepo, "/", False),
+                              "repoHistoryUrl" : self.buildHistoryUrl(userRepo) })
+      page = page + self.compileTemplate("repo_listing.html", title="browse", repos=repoList, badrepos=repoErrors)
       page = page + self.endPage()
       return page
    index.exposed = True
